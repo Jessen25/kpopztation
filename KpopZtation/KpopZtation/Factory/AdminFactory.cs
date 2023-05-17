105a8362661f04b81fcc0c72c8d871a3926cf990
kennethfilberts <kennethfilbert686@gmail.com>
2023-05-17 16:25:59 +0700

    first push

---
+++ KpopZtation/KpopZtation/Factory/AdminFactory.cs
@@ -0,0 +1,19 @@
+﻿using KpopZtation.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KpopZtation.Factory
+{
+    public class AdminFactory
+    {
+
+        public static void createAdmin(String name, String email, String gender, String address, String pass, String role)
+        {
+            ModelKpopEntities user = new ModelKpopEntities();
+
+        }
+
+    }
+}
